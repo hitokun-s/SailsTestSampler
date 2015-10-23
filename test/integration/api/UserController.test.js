c32e7673e0c03a5c212aee7029b1f47197c193e6
hitokun-s <jgpuauno@gmail.com>
2015-10-23 16:08:18 +0700

    controllerの参照方法をsails経由に変更、build-test-dataに挑戦

--- test/integration/api/UserController.test.js
+++ test/integration/api/UserController.test.js
@@ -7,30 +7,38 @@ var assert = require('assert');
 var async = require('async');
 var sinon = require("sinon");
 var stubs = require('../../stubs.js');
-var UserController = require('../../../api/controllers/UserController'); // sails独自の方法でControllerを参照できるらしい？
+//var UserController = require('../../../api/controllers/UserController');
 
+var Sails = require('sails');
+
+// create a variable to hold the instantiated sails server
+var app;
 
 describe('UserController', function () {
   before(function(){
-
+    // すべてのModelを自動探索して下記処理を裏で行うようにできたら、sails版のbuild-test-dataプラグインになるかも？
+    User.build = function(params){
+      // 実際はここでUserのプロパティとbindしたい。
+      return params;
+    }
   });
   describe('findById', function () {
     it('findbyId should return User', function (done) {
 
-      var mockUser = {id:123, name :"hitokun"};
+      //var mockUser = {id:123, name :"hitokun"};
+      var mockUser = User.build({id:123, name :"hitokun"});//なんちゃってbuild-test-data風
 
-      //sinon.stub(UserService, 'findById').returns(mockUser);
-      //sinon.stub(UserService, 'findById', function(id, callback){
-      //  callback(mockUser);
-      //});
+      // この呼び出しを進化させれば、grails * spockの、@TestFor(UserController)　のようなこともできるかも？
+      var controller = sails.controllers.user;
 
       // Stubを作成
       sinon.stub(User, 'findById').callsArgWith(1, null, mockUser);
 
-      //var req = {param: {id:123}}; // TODO 実ソース側で、params.all()とかしていたら、これだと対応できないな
       var req = {param:sinon.stub().returns(123)}; // TODO これは、実ソース側で、req.param("id")としている場合にしか対応できなさそう。。
+      // params.all()　とかする場合もありうる。
       var res = {json: sinon.spy()};
-      UserController.findById(req, res);
+      //UserController.findById(req, res);
+      controller.findById(req, res);
 
       // Grailsのような、assert res.json.id == 123 という方法の代わりに、res.json()の引数が何だったjか、をチェックする作戦。
       assert(res.json.calledWith(200, {id:123, name: "hitokun"}));
@@ -52,5 +60,8 @@ describe('UserController', function () {
       //  });
 
     });
+  });
+  after(function(){
+    //app.lower(done);
   });
 });
